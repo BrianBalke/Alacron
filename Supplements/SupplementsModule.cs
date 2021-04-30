cec90e78712d9948093bc5f22591219a659302f8
Brian Balke <brian.balke@hotmail.com>
2021-04-30 15:15:13 -0700

    Add project files.

---
+++ Supplements/SupplementsModule.cs
@@ -0,0 +1,33 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using Alacron.Supplements.Views;
+
+namespace Alacron.Supplements
+{
+    public class SupplementsModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+
+        public SupplementsModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public string Uri => "ContentRegion";
+
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            // View discovery
+            //_regionManager.RegisterViewWithRegion("ContentRegion", typeof(ViewA));
+            // View Injection
+            IRegion region = _regionManager.Regions[Uri];
+            var view1 = containerProvider.Resolve<ViewA>();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ Vitality/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Ioc;
+using System.Windows;
+using Alacron.Vitality.Views;
+using Prism.Modularity;
+using Alacron.Supplements;
+
+namespace Alacron.Vitality
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        //protected override IModuleCatalog CreateModuleCatalog()
+        //{
+        //    return new DirectoryModuleCatalog() { ModulePath = @".\Modules" };
+        //}
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<SupplementsModule>();
+        }
+    }
+}
